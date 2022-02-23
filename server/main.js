2a435323708e3570292d91fed58829c89303d771
phoeN24 <phoenix@kilodelta.com>
2022-02-23 15:34:20 -0800

    connected mogoDB

--- server/main.js
+++ server/main.js
@@ -1,44 +1,81 @@
 //imports
-const path = require('path');
+// const path = require('path');
+const express = require('express');
 const app = express();
-const roles = require('../configFiles/operations.config.gl');
+require('dotenv').config();
+
+// const roles = require('../configFiles/operations.config.gl');
+
+const controller = require('./controller');
 
 app.use(express.json());
 
 //REFACTOR
 //if the expiration date runs out on an accessToken
-app.use('/token', (req, res) => {
-    //would this work?
-    const refreshToken = req.body.token.refreshToken
-    const accessToken = req.body.token.accessToken
-    const userRole = req.body.role
+// app.use('/token', (req, res) => {
+//     //would this work?
+//     const refreshToken = req.body.token.refreshToken
+//     const accessToken = req.body.token.accessToken
+//     const userRole = req.body.role
 
-    if (accessToken === null) {
-        if (refreshToken !== null) {
-            if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
-            jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-                if (err) return res.sendStatus(403);
+//     if (accessToken === null) {
+//         if (refreshToken !== null) {
+//             if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+//             jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+//                 if (err) return res.sendStatus(403);
                 
-                accessToken = jwt.sign(userRole, secret, { expiresIn: '15m' })
-                return res.json( { accessToken: accessToken, refreshToken: refreshToken})
-            })
-        }
+//                 accessToken = jwt.sign(userRole, secret, { expiresIn: '15m' })
+//                 return res.json( { accessToken: accessToken, refreshToken: refreshToken})
+//             })
+//         }
+//     }
+// })
+
+//TO DO:
+    //Must include a userID in the DB
+    //Refactor looping endpoints to server to more accurately verify the accessToken
+    //Require in bcrypt
+    //Require in User from models
+    //Ensure req.body.user works
+
+function checkForAccessToken(req, res, next) {
+    //Must send accessToken under authorization header as a Bearer token
+    const authHeader = req.headers['authorization']
+    const accessToken = authHeader && authHeader.split(' ')[1]
+    //Go next if there is already an accessToken 
+    if (accessToken != null) {
+        res.locals.permitted = true
+        res.locals.token = accessToken
+        return next();
     }
-})
+
+    //Must send refreshToken under authorizationRefresh header as a Bearer token
+    const refreshHeader = req.headers['authorizationRefresh']
+    const refreshToken = refreshHeader && refreshHeader.split(' ')[1]
+    //If there is no refreshToken, then they will not be permitted
+    if (refreshToken == null) {
+        res.locals.permitted = false
+        return next()
+    }
+    //Check for the refreshToken in the DB
+    //User.find( { username: req.body.user }, (err, found) => {
+        
+    //})
+}
 
 //read operations and associated roles
-for (const operation in roles) {
-    app.use(`/${operation}`, (req, res) => {
-        for (const role in roles[operation]) {
-            //Do we store the secrets in a database, or in process.env?
-            const secret = `process.env.ACCESS_TOKEN_${role.toUpperCase()}_SECRET`;
-            jwt.verify(myWebToken, secret, (err, found) => {
-                if (found) return res.json(true);
-            })
-        }
-        return res.sendStatus(401);
-    });
-};
+// for (const operation in roles) {
+//     app.use(`/${operation}`, (req, res) => {
+//         for (const role in roles[operation]) {
+//             //Do we store the secrets in a database, or in process.env?
+//             const secret = `process.env.ACCESS_TOKEN_${role.toUpperCase()}_SECRET`;
+//             jwt.verify(myWebToken, secret, (err, found) => {
+//                 if (found) return res.json(true);
+//             })
+//         }
+//         return res.sendStatus(401);
+//     });
+// };
 
 //global error handling
 app.use((err, req, res, next) => {
@@ -51,7 +88,7 @@ const errObj = {...defaultError, ...err};
 return res.status(errObj.status).json(errObj.message);
 });
 
-app.listen(3000);
+app.listen(3000, () => console.log('Server listening on port: 3000'));
 
 //export
 module.exports = app;
---
+++ server/models.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.mongoURIv2, {
+    // options for the connect method to parse the URI
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // sets the name of the DB that our collections are part of
+    dbName: 'graphQLOCK-refreshTokens'
+  })
+    .then(() => console.log('Connected to Mongo DB.'))
+    .catch(err => console.log(err));
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  refreshToken: String,
+});
+
+const Users = mongoose.model('users', userSchema);
+
+module.exports = { Users };
